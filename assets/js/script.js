8afabf8d69df9c9b17c04dcbed8afdef99712236
Diogo Lopes <deosl98@gmail.com>
2021-12-21 16:58:26 +0000

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,26 +68,27 @@ function addCountriesData(data) {
             </div>
         `)
         $(row).append(card)
-    }
-    $('.countries-content').append(row)
 
-    let modal = $(`
-        <div class="modal fade" id="countriesModal"" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        ...
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+        let modal = $(`
+            <div class="modal fade ${cName}" id="countriesModal"" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">${cName}</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    `)
+        `)
+        $('.modal-container').append(modal)
+    }
+    $('.countries-content').append(row)
 }
