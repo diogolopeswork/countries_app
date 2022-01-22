fd63875a31d92cee7ab574a99b6b7732c4956f40
Diogo Lopes <deosl98@gmail.com>
2022-01-22 14:11:16 +0000

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,14 @@ $(function () {
             let val = $('#search-country').val()
             getCountry(val)
         }
-        
     })
 
-    /* $(document).on('click', '.countries-content .col', function (e) {
+    $(document).on('click', 'a.dropdown-item', function(e) {
+        let val = $(this).data('target')
+        filterRegion(val)
+    })
+
+    $(document).on('click', '.countries-content .col', function (e) {
         let target = $(this).data('target')
 
         $('.countries-container').hide('slide', {
@@ -34,7 +38,7 @@ $(function () {
         $('.countries-modal').hide('slide', {
             direction: 'left'
         }, 500)
-    }) */
+    })
 })
 
 function getData() {
@@ -51,56 +55,43 @@ function getData() {
 function getCountry(val) {
     if ($('#search-country').val() !== '') {
         $.get(`https://restcountries.com/v3.1/name/${val}`)
-            .then(response => addCountriesData(response))
+            .then(function(response) {
+                addCountriesData(response)
+            })
 
             .fail(function() {
                 displayError(msg = "Couldn't Find The Country!")
             })
     } else {
         displayError(msg = "Couldn't Find The Country!")
+        setTimeout(() => {
+            getData()
+            $('.err-fetch').remove()
+        }, 2000)
     }
 
     $('.countries-content').empty()
 }
 
-function displayError(msg) {
-    let elem = $(`
-        <div class="err-fetch px-3">
-            <h6 class="text-danger">${msg}</h2>
-        </div>
-    `)
+function filterRegion(val) {
+    $.get(`https://restcountries.com/v3.1/region/${val}`)
+        .then(function(data) {
+            addCountriesData(data)
+        })
 
-    if($('.err-fetch').length >= 1) {
-        $('.err-fetch').first().remove()
-    }
-    $('.countries-container').append(elem)
+        .fail(function() {
+            displayError(msg = "Couldn't Load The Countries!")
+        })
+        
+    $('.countries-content').empty()
 }
-
-/* function addCountry(country) {
-    let row = $(`<div class="row countries-row w-100"></div>`)
-    let card = $(`
-        <div class="col m-3" data-target="${country.alpha3Code}">
-            <div class="countries-card">
-                <div class="countries-img"><img src="${country.flags.png}" alt=""></div>
-                <div class="countries-card-details">
-                    <h5 class="countries-name fw800 mt-3">${country.name.common}</h5>
-                    <p class="countries-details fw600 m-0">Population: <span class="countries-stats">${country.population.toLocaleString()}</span></p>
-                    <p class="countries-details fw600 m-0">Region: <span class="countries-stats">${country.region}</span></p>
-                    <p class="countries-details fw600 m-0">Capital: <span class="countries-stats">${country.capital}</span></p>
-                </div>
-            </div>
-        </div>
-    `)
-    $('.countries-content').append(row)
-    $(row).append(card)
-} */
 
 function addContinents() {
     let arr = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
 
     for (let i = 0; i < arr.length; i++) {
         let elem = $(`
-            <li><a class="dropdown-item" href="#">${arr[i]}</a></li>
+            <li><a class="dropdown-item" href="#" data-target="${arr[i]}">${arr[i]}</a></li>
         `)
         $('.dropdown-menu').append(elem)
     }
@@ -131,24 +122,20 @@ function addCountriesData(data) {
                 </div>
             </div>
         `)
-    /*     let card = $(`
-            <div class="col m-3" data-target="${cName}">
-                <div class="countries-card">
-                    <div class="countries-card-top>
-                        <img src="${cFlag}" alt="">
-                    </div>
-                    <div class="countries-card-bottom>
-                        <div class="countries-card-details">
-                            <h5 class="countries-name fw800 mt-3">${cName}</h5>
-                            <p class="countries-details fw600 m-0">Population: <span class="countries-stats">${cPopulation}</span></p>
-                            <p class="countries-details fw600 m-0">Region: <span class="countries-stats">${cContinent}</span></p>
-                            <p class="countries-details fw600 m-0">Capital: <span class="countries-stats">${cCapital}</span></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `) */
         $(row).append(card)
     }
     $('.countries-content').prepend(row)
+}
+
+function displayError(msg) {
+    let elem = $(`
+        <div class="err-fetch px-3">
+            <h6 class="text-danger">${msg}</h2>
+        </div>
+    `)
+
+    if($('.err-fetch').length >= 1) {
+        $('.err-fetch').first().remove()
+    }
+    $('.countries-container').append(elem)
 }
